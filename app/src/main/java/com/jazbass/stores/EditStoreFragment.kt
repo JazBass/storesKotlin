0ee54c8222ea775f5a01031a621622bdb7b14953
JazBass <javier.bassanelli@gmail.com>
2022-09-13 12:31:32 +0200

    First commit

---
+++ app/src/main/java/com/jazbass/stores/EditStoreFragment.kt
@@ -0,0 +1,105 @@
+package com.jazbass.stores
+
+import android.content.Context
+import android.os.Bundle
+import android.view.*
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.jazbass.stores.databinding.FragmentEditStoreBinding
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+
+class EditStoreFragment : Fragment() {
+
+    private lateinit var mBinding: FragmentEditStoreBinding
+    private var mActivity: MainActivity? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        mBinding = FragmentEditStoreBinding.inflate(inflater, container, false)
+        return mBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val id = arguments?.getLong(getString(R.string.arg_id), 0)
+        if (id != null && id!=0L){
+            Toast.makeText(activity, id.toString(), Toast.LENGTH_SHORT).show()
+        }else Toast.makeText(activity, id.toString(), Toast.LENGTH_SHORT).show()
+
+        mActivity = activity as? MainActivity
+        mActivity?.supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        mActivity?.supportActionBar?.title = getString(R.string.edit_store_title)
+
+        setHasOptionsMenu(true)
+
+        mBinding.etPhotoUrl.addTextChangedListener {
+            Glide.with(this)
+                .load(mBinding.etPhotoUrl.text.toString())
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .centerCrop()
+                .into(mBinding.imagePhoto)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_save, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                mActivity?.onBackPressed()
+                true
+            }
+            R.id.action_save -> {
+                val store = StoreEntity(
+                    name = mBinding.etName.text.toString().trim(),
+                    phone = mBinding.etPhone.text.toString().trim(),
+                    website = mBinding.etWebsite.text.toString().trim(),
+                    photoUrl = mBinding.etPhotoUrl.text.toString().trim()
+                )
+                doAsync {
+                    store.id = StoreApplication.database.storeDao().addStore(store)
+                    uiThread {
+                        mActivity?.addStore(store)
+                        hideKeyword()
+                        Toast.makeText(
+                            mActivity, R.string.edit_store_message_save_success,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        mActivity?.onBackPressed()
+                    }
+                }
+                true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun hideKeyword() {
+        val imn = mActivity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imn.hideSoftInputFromWindow(requireView().windowToken, 0)
+    }
+
+    override fun onDestroyView() {
+        hideKeyword()
+        super.onDestroyView()
+    }
+
+    override fun onDestroy() {
+        mActivity?.supportActionBar?.setDisplayHomeAsUpEnabled(false)
+        mActivity?.supportActionBar?.title = getString(R.string.app_name)
+        mActivity?.hideFab(true)
+        setHasOptionsMenu(false)
+        super.onDestroy()
+    }
+}
